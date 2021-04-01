e691c637feb6f479e60b96d0bfa80e22f516ac3e
SadS4ndWiCh <caiocamargo007@gmail.com>
2021-04-01 19:58:22 -0300

    add: Profile Controller

---
+++ src/controllers/ProfileController.js
@@ -0,0 +1,30 @@
+const Profile = require('../models/Profile');
+
+module.exports = {
+  index(req, res) {
+    return  res.render('profile', { profile: Profile.get() })
+  },
+
+  update(req, res) {
+    // Novos dados do perfil
+    const data = req.body;
+
+    // Total de semanas em um ano
+    const weeksPerYear = 52;
+    // Remover as semanas de férias do ano para pegar quantas semanas tem em 1 mês
+    const weeksPerMonth = (weeksPerYear - data['vacation-per-year']) / 12; // Em média
+    // Total de horas trabalhadas na semana
+    const weekTotalHours = data['hours-per-day'] * data['days-per-week'];      
+
+    // Horas trabalhadas no mês
+    const monthlyTotalHours = weekTotalHours * weeksPerMonth;
+
+    // Qual será o valor da minha hora
+    data['value-hour'] = data['monthly-budget'] / monthlyTotalHours;
+
+    // Atualizar o data do perfil
+    Profile.update(data);
+
+    return res.redirect('/profile')
+  },
+}
