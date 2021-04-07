524d58dd50459d8ebe0070818833f08bb1f6f013
SadS4ndWiCh <caiocamargo007@gmail.com>
2021-04-07 17:36:42 -0300

    refactor: model function to use database

--- src/models/Job.js
+++ src/models/Job.js
@@ -1,61 +1,56 @@
-let data = [
-  {
-    id: 1,
-    name: 'Pizzaria Guloso',
-    'daily-hours': 2,
-    'total-hours': 1,
-    created_at: Date.now(),
-  },
-  {
-    id: 2,
-    name: 'OneTwo Project',
-    'daily-hours': 3,
-    'total-hours': 45,
-    created_at: Date.now(),
-  },
-];
+const Database = require('../utils/DatabaseUtils');
 
 module.exports = {
-  get() { return data },
+  async get() {
+    const jobsData = await Database.getAll(`
+      SELECT * from jobs
+    `);
 
-  create(newJob) {    
-    const lastId = data[data.length - 1]?.id || 0;
+    return jobsData.map(job => ({
+      ...job,
+      'daily-hours': job.daily_hours,
+      'total-hours': job.total_hours,
+    }))
+  },
 
-    data.push({
+  async create(newJob) {
+    await Database.exec(`
+      INSERT INTO jobs (
+        name,
+        daily_hours,
+        total_hours,
+        created_at
+      ) VALUES (
+        "${newJob.name}",
+        ${newJob['daily-hours']},
+        ${newJob['total-hours']},
+        ${Date.now()}
+      )
+    `);
+
+    /* data.push({
       id: lastId + 1,
       name: newJob.name,
       'daily-hours': newJob['daily-hours'],
       'total-hours': newJob['total-hours'],
       created_at: Date.now(), 
-    });
+    }); */
   },
 
-  update(jobId, newJobData) {
-    const job = data.find(job => Number(jobId) === Number(job.id));
-
-    // Caso um valor necessário não tenha sido definido
-    // na requisição, será apenas atribuído o valor antigo.
-    // `newJobData.name || job.name` -> Pegue `name` dentro
-    // de `newJobData` ou se não haver, pegue `name` do `job` 
-    const updatedJob = {
-      ...job,
-      name: newJobData.name || job.name,
-      'total-hours': newJobData['total-hours'] || job['total-hours'],
-      'daily-hours': newJobData['daily-hours'] || job['daily-hours'],
-    }
-
-    data = data.map(job => {
-      if(job.id === Number(jobId)) job = updatedJob;
-
-      return job
-    });
+  async update(jobId, newJobData) {
+    await Database.exec(`
+      UPDATE jobs SET
+        name = "${newJobData.name}",
+        daily_hours = ${newJobData['daily-hours']},
+        total_hours = ${newJobData['total-hours']}
+      WHERE
+        id = ${jobId}
+    `);
   },
 
-  delete(jobId) {
-    // Um meio de excluir que usa o `.filter()` para filtrar
-    // apenas os que não são o que quer tirar... Ou seja,
-    // vai retornar apenas os elementos que não seja o que quer
-    // excluir, tendo assim, um Array novo sem o que deseja excluir
-    data = data.filter(job => Number(jobId) !== Number(job.id));
+  async delete(jobId) {
+    await Database.exec(`
+      DELETE FROM jobs WHERE id = ${jobId}
+    `);
   }
 };
