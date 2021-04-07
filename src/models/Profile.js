eb26bec2a5ebbc5eed792d07f41df4e37c60740c
SadS4ndWiCh <caiocamargo007@gmail.com>
2021-04-07 17:19:18 -0300

    refactor: get profile data && update profile data by database

--- src/models/Profile.js
+++ src/models/Profile.js
@@ -1,15 +1,31 @@
-let data =  {
-  name: 'SadS4ndWiCh',
-  avatar: 'https://github.com/SadS4ndWiCh.png',
-  'monthly-budget': 4500,
-  'hours-per-day': 6,
-  'days-per-week': 4,
-  'vacation-per-year': 6,
-  'value-hour': 75
-};
+const Database = require('../utils/DatabaseUtils');
 
 module.exports = {
-  get() { return data },
+  async get() {
+    const profileData = await Database.getOne(`
+      SELECT * FROM profile
+    `);
+
+    return {
+      ...profileData,
+      'monthly-budget': profileData.monthly_budget,
+      'hours-per-day': profileData.hours_per_day,
+      'days-per-week': profileData.days_per_week,
+      'vacation-per-year': profileData.vacation_per_year,
+      'value-hour': profileData.value_hour
+    }
+  },
   
-  update(newData) { data = newData; },
+  async update(updatedData) {
+    await Database.exec(`
+      UPDATE profile SET
+        name = "${updatedData.name}",
+        avatar = "${updatedData.avatar}",
+        monthly_budget = ${updatedData['monthly-budget']},
+        days_per_week = ${updatedData['days-per-week']},
+        hours_per_day = ${updatedData['hours-per-day']},
+        vacation_per_year = ${updatedData['vacation-per-year']},
+        value_hour = ${updatedData['value-hour']}
+    `)
+  },
 };
